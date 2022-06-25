b4b900415b30316703ccb23f909b0d4e6d370d73
mhQ <mhudyx@gmail.com>
2022-06-25 18:57:11 +0200

    27 [FRONT] Style for UiPost

--- frontend/src/components/ui/Post/Component.vue
+++ frontend/src/components/ui/Post/Component.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="ui-post">
-    <h1>Title - {{ post.title }}</h1>
+    <div class="ui-post__title">
+      <div>{{ post.title }}</div>
+      <div class="ui-post__buttons">
+        <button
+          @click="updateButtonHandler"
+        >
+          Update
+        </button>
+        <button
+          @click="deleteButtonHandler"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+    <div class="ui-post__content">
+      {{ post.content }}
+    </div>
+    <div class="ui-post__date">
+      Opublikowano - {{ post.public_date }}
+    </div>
+    <div class="ui-post__comments">
+      0 Komentarzy
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { toRefs } from 'vue';
+import router from '../../../router';
 
 const props = defineProps({
   post: {
@@ -17,6 +41,15 @@ const props = defineProps({
 const {
   post,
 } = toRefs(props);
+
+const updateButtonHandler = (): void => {
+  console.log('Update');
+};
+
+const deleteButtonHandler = (): void => {
+  const confirmAction = confirm('Czy na pewno chcesz usunąć dany post?');
+  if (confirmAction) router.push({ path: '/' });
+};
 
 </script>
 
