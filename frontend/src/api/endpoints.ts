8fd124f0a78f10a36f2f62c839d695d3aa132127
Mateusz H <52915047+mhQ260@users.noreply.github.com>
2022-06-25 20:34:44 +0200

    31 [FRONT] Attaching the delete method to the front (#32)

--- frontend/src/api/endpoints.ts
+++ frontend/src/api/endpoints.ts
@@ -1,3 +1,4 @@
 export const URI_POSTS = 'posts/';
 export const URI_POST_ADD = 'post/add';
 export const URI_POST_ID = (id: number) => `post/${id}/`;
+export const URI_POST_DELETE_ID = (id: number) => `post/delete/${id}/`;
