28f342d1ba856a8e81c730b26662b00d5fc848d2
Mateusz H <52915047+mhQ260@users.noreply.github.com>
2022-06-25 17:24:05 +0200

    21 Create view for single post (#22)

--- frontend/src/api/endpoints.ts
+++ frontend/src/api/endpoints.ts
@@ -1,2 +1,3 @@
 export const URI_POSTS = 'posts/';
 export const URI_POST_ADD = 'post/add';
+export const URI_POST_ID = (id: number) => `post/${id}`;
--- frontend/src/views/Post/Component.vue
+++ frontend/src/views/Post/Component.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="post">
-    <h1>
-      Post
-    </h1>
+    <UiPost 
+      :post="APIData"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { UiPost } from '../../components';
+import { URI_POST_ID } from '../../api/endpoints';
+import { getAPI } from '../../api/http';
+
+const APIData = ref({});
+
+onMounted(() => {
+  getAPI.get(URI_POST_ID(1))
+    .then(res => {
+      APIData.value = res.data;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
 
 </script>
