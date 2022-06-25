aab7c18efd84009fff16755fb210568263e18c05
Mateusz H <52915047+mhQ260@users.noreply.github.com>
2022-06-25 20:20:06 +0200

    29 [BACK] Post delete method (#30)

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . views import PostView, PostDetailView
+from . views import PostView, PostDetailView, PostDelete
 
 urlpatterns = [
     path('posts/', PostView.as_view(), name='posts_view'),
     path('post/add', PostView.as_view(), name='add_post_view'),
     path('post/<int:post_id>/', PostDetailView.as_view(), name='get_post_view'),
+    path('post/delete/<int:post_id>/', PostDelete.as_view(), name='delete_post'),
 ]
