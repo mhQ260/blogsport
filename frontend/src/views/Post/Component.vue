1739d7105eee7e60982eab286bd12905776c640f
Mateusz H <52915047+mhQ260@users.noreply.github.com>
2022-06-25 18:25:58 +0200

    25 [FRONT] Fix passing posts id value (#26)

--- frontend/src/views/Post/Component.vue
+++ frontend/src/views/Post/Component.vue
@@ -11,11 +11,15 @@ import { onMounted, ref } from 'vue';
 import { UiPost } from '../../components';
 import { URI_POST_ID } from '../../api/endpoints';
 import { getAPI } from '../../api/http';
+import { useRoute } from 'vue-router';
 
 const APIData = ref({});
 
+const route = useRoute();
+const id = route.params.id;
+
 onMounted(() => {
-  getAPI.get(URI_POST_ID(1))
+  getAPI.get(URI_POST_ID(Number(id)))
     .then(res => {
       APIData.value = res.data;
     })
