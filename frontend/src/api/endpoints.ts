bc6eb50ab8d6fe677ba3f5096354d63fffda0aae
Mateusz H <52915047+mhQ260@users.noreply.github.com>
2022-06-27 17:13:18 +0200

    33 [FRONT] Create update post view (#34)

--- frontend/src/api/endpoints.ts
+++ frontend/src/api/endpoints.ts
@@ -2,3 +2,4 @@ export const URI_POSTS = 'posts/';
 export const URI_POST_ADD = 'post/add';
 export const URI_POST_ID = (id: number) => `post/${id}/`;
 export const URI_POST_DELETE_ID = (id: number) => `post/delete/${id}/`;
+export const URI_POST_UPDATE = (id: number) => `post/update/${id}/`;
--- frontend/src/components/ui/Post/Component.vue
+++ frontend/src/components/ui/Post/Component.vue
@@ -2,18 +2,6 @@
   <div class="ui-post">
     <div class="ui-post__title">
       <div>{{ post.title }}</div>
-      <div class="ui-post__buttons">
-        <button
-          @click="updateButtonHandler"
-        >
-          Update
-        </button>
-        <button
-          @click="deleteButtonHandler"
-        >
-          Delete
-        </button>
-      </div>
     </div>
     <div class="ui-post__content">
       {{ post.content }}
@@ -29,9 +17,6 @@
 
 <script setup lang="ts">
 import { toRefs } from 'vue';
-import router from '../../../router';
-import { URI_POST_DELETE_ID } from '../../../api/endpoints';
-import { getAPI } from '../../../api/http';
 
 const props = defineProps({
   post: {
@@ -43,26 +28,6 @@ const props = defineProps({
 const {
   post,
 } = toRefs(props);
-
-const updateButtonHandler = (): void => {
-  console.log('Update');
-};
-
-const deleteFunc = async (): Promise<void> => {
-  try {
-    await getAPI.post(URI_POST_DELETE_ID(Number(post.value.id)));
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const deleteButtonHandler = (): void => {
-  const confirmAction = confirm('Czy na pewno chcesz usunąć dany post?');
-  if (confirmAction) {
-    deleteFunc();
-    router.push({ path: '/' });
-  }
-};
 
 </script>
 
