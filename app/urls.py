240a9259c655fd245bd51a65d30009441474c953
mhQ <mhudyx@gmail.com>
2022-06-27 17:15:11 +0200

    35 [BACK] Create method for update post

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('post/add', PostView.as_view(), name='add_post_view'),
     path('post/<int:post_id>/', PostDetailView.as_view(), name='get_post_view'),
     path('post/delete/<int:post_id>/', PostDelete.as_view(), name='delete_post'),
+    path('post/update/<int:post_id>/', PostDetailView.as_view(), name='update_post_view'),
 ]
